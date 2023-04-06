b6150051bf4cff3ec224ef2dfaf6c4c968b790a2
Ilan Cosman <ilancosman@gmail.com>
2023-04-06 08:10:03 -0700

    Use string matching in distrobox item

--- functions/_tide_item_distrobox.fish
+++ functions/_tide_item_distrobox.fish
@@ -1,4 +1,6 @@
 function _tide_item_distrobox
-    test -e /etc/profile.d/distrobox_profile.sh && test -e /run/.containerenv &&
-        _tide_print_item distrobox $tide_distrobox_icon' ' (string match -rg 'name="(.*)"' </run/.containerenv)
+    if test -e /etc/profile.d/distrobox_profile.sh && test -e /run/.containerenv
+        string match -qr 'name="(?<_distrobox_name>.*)"' </run/.containerenv
+        _tide_print_item distrobox $tide_distrobox_icon' ' $_distrobox_name
+    end
 end
--- functions/_tide_item_pulumi.fish
+++ functions/_tide_item_pulumi.fish
@@ -1,18 +1,18 @@
 function _tide_item_pulumi
     if path filter $_tide_parent_dirs/Pulumi.yaml | read -l yaml_path
         if command -q sha1sum
-            echo -n "$yaml_path" | sha1sum | string match -qr "(?<path_hash>.{40})"
+            echo -n "$yaml_path" | sha1sum | string match -qr "(?<_path_hash>.{40})"
         else if command -q shasum
-            echo -n "$yaml_path" | shasum | string match -qr "(?<path_hash>.{40})"
+            echo -n "$yaml_path" | shasum | string match -qr "(?<_path_hash>.{40})"
         end
 
-        if test -n "$path_hash"
-            string match -qr 'name: *(?<project_name>.*)' <$yaml_path
-            set -l workspace_file "$HOME/.pulumi/workspaces/$project_name-$path_hash-workspace.json"
+        if test -n "$_path_hash"
+            string match -qr 'name: *(?<_project_name>.*)' <$yaml_path
+            set -l workspace_file "$HOME/.pulumi/workspaces/$_project_name-$_path_hash-workspace.json"
 
             if test -e $workspace_file
-                string match -qr '"stack": *"(?<stack>.*)"' <$workspace_file
-                _tide_print_item pulumi $tide_pulumi_icon' ' $stack
+                string match -qr '"stack": *"(?<_stack>.*)"' <$workspace_file
+                _tide_print_item pulumi $tide_pulumi_icon' ' $_stack
             end
         end
     end
